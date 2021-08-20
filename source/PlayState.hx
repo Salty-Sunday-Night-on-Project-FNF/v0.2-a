f236ec46ad9a1a354aa72831622f2c60e3f7c488
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2021-08-20 09:05:36 -0600

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -49,22 +49,26 @@ class PlayState extends MusicBeatState
 	var characterCol:Array<String> = CoolUtil.coolTextFile(Paths.txt('characterList'));
 	var col:Array<FlxColor> = [
 		0xFF51d8fb, // BF
+		0xFF51d8fb, // BETA-BF
 		0xFF9fe6ff, // BF-PIXEL
 		0xFF51d8fb, // BF-CHRISTMAS
 		0xFF51d8fb, // BF-CAR
-		0xFFca1f6f, // GF
-		0xFFca1f6f, // GF-CHRISTMAS (Pretty sure this one doesn't really do anything.)
-		0xFFc885e5, // DAD
+		0xFFec7aac, // GF
+		0xFFec7aac, // GF-CHRISTMAS (Pretty sure this one doesn't really do anything.)
+		0xFF434141, // DAD
 		0xFFec7aac, // MOM
-		0xFFec7aac, // MOM-CAR
-		0xFFffffff, // PARENTS-CHRISTMAS (Look I don't know what a Better color would be.)
-		0xFFf9a326, // SPOOKY
-		0xFFceec75, // PICO
-		0xFFf5ff8a, // MONSTER
-		0xFFf5ff8a, // MONSTER-CHRISTMAS
-		0xFFffaa6f, // SENPAI
-		0xFFffaa6f, // SENPAI-ANGRY
-		0xFFff5d87 // SPIRIT
+		0xFFfa95a9, // MOM-CAR
+		0xFFfa95a9, // BETA-MOM
+		0xFFff0000, // PARENTS-CHRISTMAS
+		0xFFff98ff, // SPOOKY
+		0xFFff98ff, // BETA-SPOOKY
+		0xFF3366ff, // PICO
+		0xFF000000, // MONSTER
+		0xFF000000, // MONSTER-CHRISTMAS
+		0xFF4cff00, // SENPAI
+		0xFF4cff00, // SENPAI-ANGRY
+		0xFF0da554 // SPIRIT
+		0xFF0da554 // SPIRIT-SUS
 	];
 
 	/* 
