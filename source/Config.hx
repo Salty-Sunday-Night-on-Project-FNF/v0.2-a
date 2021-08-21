ed79871c5f315058b866bbfccee4efbf3943ce16
Derpy-Jacob-903 <73241159+Derpy-Jacob-903@users.noreply.github.com>
2021-08-21 08:49:48 -0500

    Update Config.hx

--- source/Config.hx
+++ source/Config.hx
@@ -2,8 +2,8 @@
 // These are config options the player doesn't get to access so you can keep the game looking as intended.
 class Config extends MusicBeatState
 {
-	public static var CONFIGGfCar:Bool = true; // If you want GF on week 4 to be sitting on the car in the back and not on a speaker.
-	public static var CONFIGTitle:Bool = true; // Title screen uses ProjectFNF logo instead of the Logo bumpin.
+	public static var CONFIGGfCar:Bool = false; // If you want GF on week 4 to be sitting on the car in the back and not on a speaker.
+	public static var CONFIGTitle:Bool = false; // Title screen uses ProjectFNF logo instead of the Logo bumpin.
 	public static var CONFIGSkip:Bool = true; // Skips the "You are using a bla bla bla projectfnf bla bla bla github bla bla bla" message
-	public static var ModName:String = 'ProjectFNF'; // Change this to the name of your mod
+	public static var ModName:String = 'Saltys Sunday Night'; // Change this to the name of your mod
 }
